94f100415483ff63e499bd9cac7521c8e069fd4e
savinay1 <31352945+savinay1@users.noreply.github.com>
2018-09-14 22:28:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,3 +124,59 @@ def describe(model_name):
 
 
     return ("The "+model_name +" model "  " has "+str(Root).strip('[]')+" as " +op1+ " and "+str(Leaf).strip('[]')+" as "+op+". " +s+s2)
+
+def infer(model_name,output_node,observe):
+    '''description: query the given bayesian model
+    Parameters
+        ----------
+        model: pgmpy Bayesian Object
+        returns: result of the given query
+    '''
+    class SimpleInference(Inference):
+        ''' custom inference'''
+        def query(self, var, evidence):
+            # self.factors is a dict of the form of {node: [factors_involving_node]}
+            factors_list = set(itertools.chain(*self.factors.values()))
+            product = factor_product(*factors_list)
+            reduced_prod = product.reduce(evidence, inplace=False)
+            reduced_prod.normalize()
+            var_to_marg = set(self.model.nodes()) - set(var) - set([state[0] for state in evidence])
+            marg_prod = reduced_prod.marginalize(var_to_marg, inplace=False)
+            return marg_prod
+
+    if not model_present(model_name):
+        return "Model not found"
+
+    # getting model pickle
+    model_pickle = "./examples/models/"+model_name+".pickle"
+    pickle_in = open(model_pickle, "rb")
+    model = pickle.load(pickle_in)
+
+    # getting model json
+    model_json_file = "./examples/data/"+model_name+".json"
+    with open(model_json_file) as f:
+        model_json = json.load(f)
+
+    m = model_json["nodes"]
+
+    #for infer
+    for key in observe:
+        for node in m:
+            if key==m[node]:
+                observe[node]=observe[key]
+                del observe[key]
+    #getting mapping for output node
+    for node in m:
+        if m[node]==output_node:
+            outp_nde=node
+
+    # giving evidence (array of tuples)
+    evidence_array =observe.items()
+    # infer object
+    infer = SimpleInference(model)
+    # working for only one evidence
+    result = infer.query(var=outp_nde, evidence=evidence_array).values[1]
+
+    output_value={'outp_v':str(result)}
+
+    return (json.dumps(output_value))
